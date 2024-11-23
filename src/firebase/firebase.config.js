c1319958e845e7cd4f908741595da1637bfd4413
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2024-11-23 23:28:23 +0600

    added authprovider and login , register page

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD1ZDV_Iwg4KXfRGAulXipj9P8WPzyvbgo",
+  authDomain: "worksphere-8802e.firebaseapp.com",
+  projectId: "worksphere-8802e",
+  storageBucket: "worksphere-8802e.firebasestorage.app",
+  messagingSenderId: "850613955340",
+  appId: "1:850613955340:web:1a62a5ea9cff178b6ef729"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
