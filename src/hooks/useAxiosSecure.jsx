69f10aea766a86cf692dd60b60f6e788f3ded472
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-01-17 23:04:26 +0600

    added axiosSecure (interceptors)

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,11 +1,32 @@
 import axios from "axios";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
 })
 const useAxiosSecure = () => {
+    const {logout} = useAuth();
+    const navigate = useNavigate();
     
+    // Response Interceptor
+    axiosSecure.interceptors.response.use(
+        res => {
+            // console.log('response from:', res);
+            return res;
+        },
+        async error => {
+            console.log('error from axios interceptors', error.response);
+            if(error.response.status === 401 || error.response.status === 403) {
+                await logout();
+                navigate('/login');
+            } 
+            return Promise.reject(error);
+        }
+    )
+
+
     return axiosSecure
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import { RouterProvider } from 'react-router-dom'
 import router from './routes/Routes'
 import AuthProvider from './provider/AuthProvider'
 import { Toaster } from 'react-hot-toast'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-
+const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
+    <QueryClientProvider client={queryClient}>
     <RouterProvider router={router}></RouterProvider>
+    </QueryClientProvider>
     <Toaster></Toaster>
     </AuthProvider>
   </StrictMode>,
